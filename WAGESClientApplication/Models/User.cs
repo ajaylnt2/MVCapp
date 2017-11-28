df76d15fbe93b824d2a10af08fce0b735682adb2
Ajay Kumar  Kosuru <AjayKumar.Kosuru_ext@Lnttechservices.com>
2017-11-28 16:26:32 +0530

    MVC application

---
+++ WAGESClientApplication/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WAGESClientApplication.Models
+{
+    public class User
+    {
+        #region public properties
+        [Required(ErrorMessage ="User Name Required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage ="Password Required")]
+        public string email { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string country { get; set; }
+        public string Error { get; set; }
+        public string Password { get; set; }
+        public bool checkUser (string UserName,string email)
+        {
+            bool flag = false;
+            return flag;
+        }
+
+        #endregion
+    }
+}
